8efc0b69ce57eac0baab94b6f9c063aef7bdbbbc
a-deep97 <a.deep97@outlook.com>
2021-01-27 16:18:14 +0530

    main index page and stylesheet introduced

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const express = require('express')
 
 const app =express();
+const request=require('request')
 
-app.use('view engine','ejs')
+app.set('view engine','ejs')
 
 var city='delhi'
 var url= `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=271d1234d3f497eed5b1d80a07b3fcd1`;
 
-app.listen(8000)
+app.get('/',(req,res)=>{
+    
+    res.render('index');
+    /*
+    request(url,(error,response,body)=>{
+        weather_json=JSON.parse(body)
+        console.log(weather_json.main.temp)
+
+        let weather ={
+            city: city,
+            temperature: Math.round(weather_json.main.temp),
+            description: weather_json.weather[0].description,
+            icon: weather_json.weather[0].icon
+        }
+    })
+    */
+})
+
+
+app.listen(8000); 
